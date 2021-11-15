a0b1a80a715032e6b1dd9ab6843b6b3fd78a729d
danideu <danideu@gmail.com>
2021-11-15 01:11:53 +0100

    Tema3 - Php Basics - Nivell 1  Exercise 4

---
+++ nivell-1/ejercicio4.php
@@ -0,0 +1,53 @@
+<?php
+$X = 10;
+$Y = 5;
+$N = 1.1;
+$M = 2.2;
+
+echo "El valor de X: " . $X;
+echo "<br>";
+echo "El valor de Y: " . $Y;
+echo "<br>";
+echo "La suma de X+Y: " . ($X+$Y);
+echo "<br>";
+echo "La resta de X-Y: " . ($X-$Y);
+echo "<br>";
+echo "El producto de X*Y: " . ($X*$Y);
+echo "<br>";
+echo "El módulo de X%Y: " . ($X%$Y);
+echo "<br>";
+
+echo "<hr>";
+
+echo "El valor de N: " . $N;
+echo "<br>";
+echo "El valor de M: " . $M;
+echo "<br>";
+echo "La suma de N+M: " . ($N+$M);
+echo "<br>";
+echo "La resta de N-M: " . ($N-$M);
+echo "<br>";
+echo "El producto de N*M: " . ($N*$M);
+echo "<br>";
+echo "El módulo de N%M: " . ($N%$M);
+echo "<br>";
+
+echo "<hr>";
+
+echo "El doble de X: " . ($X*2);
+echo "<br>";
+echo "El doble de Y: " . ($Y*2);
+echo "<br>";
+echo "El doble de N: " . ($N*2);
+echo "<br>";
+echo "El doble de M: " . ($M*2);
+echo "<br>";
+
+echo "<hr>";
+
+echo "La suma de todo: " . ($X+$Y+$N+$M);
+echo "<br>";
+echo "El producto de todo: " . ($X*$Y*$N*$M);
+
+
+?>
